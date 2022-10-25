eb4e5f03efb3ae4b63c2098035d6443249ce74b6
Pedro Santos <pedro.santos@nsinova.onmicrosoft.com>
2022-10-25 17:08:59 -0300

    Novo método service pet

--- src/app/services/services/pets.service.ts
+++ src/app/services/services/pets.service.ts
@@ -1,19 +1,31 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { firstValueFrom } from 'rxjs';
 import { Pet } from 'src/app/models/models/pet';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class PetService {
-    constructor(
-        private http: HttpClient,
-    ) { }
+  constructor(
+    private http: HttpClient,
+  ) { }
 
+  async addPet(newPet: Pet) {
+    const endpoint = `https://petstore.swagger.io/v2/pet`;
+    return this.http.post<Pet[]>(endpoint, newPet).toPromise();
 
-    async findByStatus(status: 'available' | 'pending' | 'sold') {
-        const endpoint = `https://petstore.swagger.io/v2/pet/findByStatus?status=${status}`;
-        return this.http.get<Pet[]>(endpoint).toPromise();
-    }
+  }
+
+  async deletePet(pet: Pet){
+    const endpoint = `https://petstore.swagger.io/v2/pet/${pet.id}`;
+    return firstValueFrom(this.http.delete<Pet>(endpoint));
+
+  }
+
+  async findByStatus(status: 'available' | 'pending' | 'sold') {
+    const endpoint = `https://petstore.swagger.io/v2/pet/findByStatus?status=${status}`;
+    return this.http.get<Pet[]>(endpoint).toPromise();
+  }
 
 }
